edfe644af1d187fa9c9e0c2842ee7d51a77c896f
haritsdev <teguhcodedev1@gmail.com>
2022-06-10 22:37:53 +0700

    Part 2 Multi Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,122 @@
-import 'package:belajar_bloc/color_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'cart.dart';
+import 'money.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ColorBloc bloc = ColorBloc();
-  @override
-  void dispose() {
-    bloc.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            floatingActionButton: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    bloc.eventSink.add(ColorEvent.to_amber);
-                  },
-                  backgroundColor: Colors.amber,
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Money>(
+            create: (context) => Money(),
+          ),
+          ChangeNotifierProvider<Cart>(
+            create: (context) => Cart(),
+          )
+        ],
+        child: Scaffold(
+          floatingActionButton: Consumer<Money>(
+            builder: (context, money, _) => Consumer<Cart>(
+              builder: (context, cart, _) => FloatingActionButton(
+                onPressed: () {
+                  if (money.balance >= 500) {
+                    cart.quantity += 1;
+                    money.balance -= 500;
+                  }
+                },
+                child: Icon(
+                  Icons.shopping_bag_rounded,
+                  size: 30,
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    bloc.eventSink.add(ColorEvent.to_light_blue);
-                  },
-                  backgroundColor: Colors.lightBlue,
-                ),
-              ],
+                backgroundColor: Colors.purple,
+              ),
             ),
-            appBar: AppBar(
-              title: Text("Bloc tanpa Library"),
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: Text("Multi Provider"),
+          ),
+          body: Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Balance"),
+                      Container(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Consumer<Money>(
+                            builder: (context, money, _) => Text(
+                              money.balance.toString(),
+                              style: TextStyle(
+                                  color: Colors.purple,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ),
+                        height: 30,
+                        width: 150,
+                        margin: EdgeInsets.all(5),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.purple[100],
+                            border: Border.all(color: Colors.purple)),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Consumer<Cart>(
+                        builder: (context, cart, _) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Apple (500) x  ${cart.quantity.toString()}",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              (500 * cart.quantity).toString(),
+                              style: TextStyle(
+                                  color: Colors.purple,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    height: 30,
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  )
+                ],
+              ),
             ),
-            body: Center(
-                child: StreamBuilder(
-              stream: bloc.stateStream,
-              initialData: Colors.amber,
-              builder: (context, snapshot) {
-                return AnimatedContainer(
-                  width: 100,
-                  height: 100,
-                  color: snapshot.data,
-                  duration: Duration(microseconds: 500),
-                );
-              },
-            ) //membuild widget setiap kali mendapat update dari stream
-                )));
+          ),
+        ),
+      ),
+    );
   }
 }
